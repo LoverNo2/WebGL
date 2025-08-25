ca0b10cdab5e3d972bc8b13b6a1a8406907809ab
Arca <wu.xinhai@outlook.com>
2025-08-25 17:29:49 +0800

    精简

--- js/drawTriangle.js
+++ js/drawTriangle.js
@@ -1,4 +1,5 @@
 import * as webglUtils from '../node_modules/webgl-utils.js/es/index.js'
+
 function createShader(gl, type, source) {
   var shader = gl.createShader(type)
   gl.shaderSource(shader, source)
@@ -7,6 +8,8 @@ function createShader(gl, type, source) {
   if (success) {
     return shader
   }
+
+  console.log(gl.getShaderInfoLog(shader))
   gl.deleteShader(shader)
 }
 
@@ -19,59 +22,35 @@ function createProgram(gl, vertexShader, fragmentShader) {
   if (success) {
     return program
   }
+
+  console.log(gl.getProgramInfoLog(program))
   gl.deleteProgram(program)
 }
 
 function main() {
   var canvas = document.querySelector('#webglCanvas')
   var gl = canvas.getContext('webgl')
-  if (!gl) {
-    return
-  }
 
-  // Get the strings for our GLSL shaders
   var vertexShaderSource = document.querySelector('#vertex-shader-2d').text
   var fragmentShaderSource = document.querySelector('#fragment-shader-2d').text
-
-  // create GLSL shaders, upload the GLSL source, compile the shaders
   var vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource)
   var fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource)
 
-  // Link the two shaders into a program
   var program = createProgram(gl, vertexShader, fragmentShader)
+  gl.useProgram(program)
 
-  // look up where the vertex data needs to go.
-  var positionAttributeLocation = gl.getAttribLocation(program, 'a_position')
-
-  // Create a buffer and put three 2d clip space points in it
-  var positionBuffer = gl.createBuffer()
-
-  // Bind it to ARRAY_BUFFER (think of it as ARRAY_BUFFER = positionBuffer)
-  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer)
-
-  var positions = [0, 0, 0, 0.5, 0.7, 0]
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW)
-
-  // code above this line is initialization code.
-  // code below this line is rendering code.
+  gl.bindBuffer(gl.ARRAY_BUFFER, gl.createBuffer())
+  // gl.bufferData 将顶点数据 [0, 0, 0, 0.5, 0.7, 0] 传入GPU
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([0, 0, 0, 0.5, 0.7, 0]), gl.STATIC_DRAW)
 
   webglUtils.resizeCanvasToDisplaySize(gl.canvas)
-
-  // Tell WebGL how to convert from clip space to pixels
   gl.viewport(0, 0, gl.canvas.width, gl.canvas.height)
 
-  // Clear the canvas
   gl.clearColor(0, 0, 0, 0)
   gl.clear(gl.COLOR_BUFFER_BIT)
 
-  // Tell it to use our program (pair of shaders)
-  gl.useProgram(program)
-
-  // Turn on the attribute
+  var positionAttributeLocation = gl.getAttribLocation(program, 'a_position')
   gl.enableVertexAttribArray(positionAttributeLocation)
-
-  // Bind the position buffer.
-  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer)
 
   // Tell the attribute how to get data out of positionBuffer (ARRAY_BUFFER)
   var size = 2 // 2 components per iteration
@@ -80,12 +59,14 @@ function main() {
   var stride = 0 // 0 = move forward size * sizeof(type) each iteration to get the next position
   var offset = 0 // start at the beginning of the buffer
   gl.vertexAttribPointer(positionAttributeLocation, size, type, normalize, stride, offset)
+  // 核心功能是​​建立 ​​顶点缓冲区对象（VBO）数据与着色器属性间的映射关系​​，通过参数精细化控制数据解析方式（类型、步长、归一化等）
 
-  // draw
-  var primitiveType = gl.TRIANGLES
-  var offset = 0
-  var count = 3
-  gl.drawArrays(primitiveType, offset, count)
+  gl.drawArrays(gl.TRIANGLES, 0, 3)
+  // - 1.从绑定的缓冲区中按指定顺序读取顶点数据
+  // - 2.为每个顶点执行顶点着色器
+  // - 3.基于指定的图元类型（三角形）组装顶点
+  // - 4.为每个片段执行片段着色器
+  // - 5.最终将结果渲染到画布上
 }
 
 main()
